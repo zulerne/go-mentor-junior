c5b329886b590f12e41334db70b51f2f79e34b6c
zulerne <165476940+zulerne@users.noreply.github.com>
2026-09-22 11:39:14 +0700

    feat(order): implement ready restaurant order handler and service

--- services/order/internal/services/restaurant/ready_order.go
+++ services/order/internal/services/restaurant/ready_order.go
@@ -2,12 +2,42 @@ package restaurant
 
 import (
 	"context"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/zulerne/go-mentor-junior/order/internal/domain"
 )
 
 func (r *Service) ReadyOrder(ctx context.Context, restaurantID uuid.UUID, orderID uuid.UUID) (domain.Order, error) {
-	return domain.Order{}, nil
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	order, err := r.store.Find(ctx, orderID)
+	if err != nil {
+		return domain.Order{}, err
+	}
+	if restaurantID != order.RestaurantID {
+		return domain.Order{}, domain.ErrRestaurantIDMismatch
+	}
+
+	if order.Status == domain.ReadyForPickup {
+		return order, nil
+	}
+	if order.Status != domain.Preparing {
+		return domain.Order{}, domain.ErrInvalidOrderStatus
+	}
+
+	if err := r.deliveryProvider.Start(ctx, orderID); err != nil {
+		return domain.Order{}, domain.ErrDeliveryProvider
+	}
+
+	order.Status = domain.ReadyForPickup
+	order.DeliveryStatus = domain.InDelivery
+	// TODO: remove when bd is connected
+	order.UpdatedAt = time.Now().UTC()
+	if err := r.store.Update(ctx, order); err != nil {
+		return domain.Order{}, err
+	}
+	return order, nil
 
 }
