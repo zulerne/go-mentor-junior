5bae6d53860a426b7ee9418c7b488b1b6ef1e39b
zulerne <165476940+zulerne@users.noreply.github.com>
2026-09-22 13:48:17 +0700

    feat(order): implement remove item from cart functionality

--- services/order/internal/services/customer/get_cart.go
+++ services/order/internal/services/customer/get_cart.go
@@ -7,8 +7,8 @@ import (
 	"github.com/zulerne/go-mentor-junior/order/internal/domain"
 )
 
-func (c *Service) GetCart(ctx context.Context, customerID uuid.UUID) (domain.Cart, error) {
-	cart, err := c.cartStore.FindCart(ctx, customerID)
+func (s *Service) GetCart(ctx context.Context, customerID uuid.UUID) (domain.Cart, error) {
+	cart, err := s.cartStore.FindCart(ctx, customerID)
 	if err != nil {
 		return domain.Cart{}, err
 	}
--- services/order/internal/services/customer/remove_item_from_cart.go
+++ services/order/internal/services/customer/remove_item_from_cart.go
@@ -2,11 +2,29 @@ package customer
 
 import (
 	"context"
+	"slices"
 
 	"github.com/google/uuid"
-	"github.com/zulerne/go-mentor-junior/order/internal/domain"
 )
 
-func (s *Service) RemoveItemFromCart(ctx context.Context, customerID uuid.UUID, itemID uuid.UUID) (domain.Cart, error) {
-	return domain.Cart{}, nil
+func (s *Service) RemoveItemFromCart(ctx context.Context, customerID uuid.UUID, menuItemID uuid.UUID) error {
+	cart, err := s.cartStore.FindCart(ctx, customerID)
+	if err != nil {
+		return err
+	}
+
+	for i, cartItem := range cart.Items {
+		if cartItem.MenuItemID == menuItemID {
+			cart.Items = slices.Concat(cart.Items[:i], cart.Items[i+1:])
+			cart.SubtotalMinor -= cartItem.UnitPriceMinor * int64(cartItem.Quantity)
+			break
+		}
+	}
+
+	if len(cart.Items) == 0 {
+		cart.RestaurantID = uuid.Nil
+		cart.Currency = ""
+	}
+
+	return s.cartStore.UpdateCart(ctx, customerID, cart)
 }
